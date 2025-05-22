30c3777d9c050a228927460e7f01f1c2c1b17b7a
Дарина Сергеева <darinasergeeva122@gmail.com>
2025-05-22 22:36:32 +0500

    Добавление модели пользователя

--- Pizza Mia/Models/AppContext.cs
+++ Pizza Mia/Models/AppContext.cs
@@ -31,6 +31,8 @@ public partial class AppContext : DbContext
 
     public virtual DbSet<Supplier> Suppliers { get; set; }
 
+    public DbSet<User> Users { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=bd_dishes;Username=postgres;Password=1111");
