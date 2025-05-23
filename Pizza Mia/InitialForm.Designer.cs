d551ee01526c82b4601299fcc74629329c90ac84
Дарина Сергеева <darinasergeeva122@gmail.com>
2025-05-23 23:45:57 +0500

    Создание и настройка внешнего вида начальной формы

---
+++ Pizza Mia/InitialForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Pizza_Mia
+{
+    partial class InitialForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonEnter = new Button();
+            buttonRegistration = new Button();
+            buttonMenu = new Button();
+            SuspendLayout();
+            // 
+            // buttonEnter
+            // 
+            buttonEnter.BackColor = SystemColors.Control;
+            buttonEnter.Location = new Point(267, 12);
+            buttonEnter.Name = "buttonEnter";
+            buttonEnter.Size = new Size(150, 35);
+            buttonEnter.TabIndex = 0;
+            buttonEnter.Text = "Войти";
+            buttonEnter.UseVisualStyleBackColor = false;
+            // 
+            // buttonRegistration
+            // 
+            buttonRegistration.BackColor = SystemColors.Control;
+            buttonRegistration.Location = new Point(423, 12);
+            buttonRegistration.Name = "buttonRegistration";
+            buttonRegistration.Size = new Size(150, 35);
+            buttonRegistration.TabIndex = 1;
+            buttonRegistration.Text = "Регистрация ";
+            buttonRegistration.UseVisualStyleBackColor = false;
+            // 
+            // buttonMenu
+            // 
+            buttonMenu.Location = new Point(12, 356);
+            buttonMenu.Name = "buttonMenu";
+            buttonMenu.Size = new Size(556, 37);
+            buttonMenu.TabIndex = 2;
+            buttonMenu.Text = "Меню";
+            buttonMenu.UseVisualStyleBackColor = true;
+            // 
+            // InitialForm
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.YFouxYbs8wo;
+            ClientSize = new Size(585, 405);
+            Controls.Add(buttonMenu);
+            Controls.Add(buttonRegistration);
+            Controls.Add(buttonEnter);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(5, 5, 5, 5);
+            MaximizeBox = false;
+            Name = "InitialForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Добро пожаловать!";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonEnter;
+        private Button buttonRegistration;
+        private Button buttonMenu;
+    }
+}
